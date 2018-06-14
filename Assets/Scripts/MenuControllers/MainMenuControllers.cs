bba21f1565fbb4c68241f47e05f24f0f5decf26d
dimitrios <dimitrios.khristoforidi@gmail.com>
2018-06-14 15:03:46 +0600

    Create Game. Create menu scene

---
+++ Assets/Scripts/MenuControllers/MainMenuControllers.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MainMenuControllers : MonoBehaviour {
+    private const string Name = "MainScene";
+
+    public void StartGame()
+    {
+#pragma warning disable CS0618 // Тип или член устарел
+        Application.LoadLevel(Name);
+#pragma warning restore CS0618 // Тип или член устарел
+    }
+}
