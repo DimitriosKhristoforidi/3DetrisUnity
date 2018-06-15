7c546fbb164870724f9e329bdc89e39ee1beafbb
dimitrios <dimitrios.khristoforidi@gmail.com>
2018-06-15 23:54:08 +0600

    Create Tetris Objects. Create Spawner

---
+++ Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] tetrisObjects;
+
+    private void Start()
+    {
+        SpawnRandomObject();
+    }
+
+    public void SpawnRandomObject()
+    {
+        int index = Random.Range(0, tetrisObjects.Length);
+        Instantiate (tetrisObjects[index], transform.position, Quaternion.identity);
+    }
+}
